41464053c5814275597cc943eedeea930052f12e
unknown <you@example.com>
2022-02-05 23:32:15 +0600

    first commit

---
+++ lib/View/world_states.dart
@@ -0,0 +1,210 @@
+import 'package:covid_19/ViewModel/world_sates_view_model.dart';
+import 'package:covid_19/Model/world_states.dart';
+import 'package:covid_19/View/countries_list_screen.dart';
+import 'package:covid_19/ViewModel/world_sates_view_model.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'package:pie_chart/pie_chart.dart';
+
+class WorldStates extends StatefulWidget {
+  const WorldStates({Key? key}) : super(key: key);
+
+  @override
+  _WorldStatesState createState() => _WorldStatesState();
+}
+
+class _WorldStatesState extends State<WorldStates>
+    with TickerProviderStateMixin {
+  late final AnimationController _controller = AnimationController(
+    duration: const Duration(milliseconds: 1000),
+    vsync: this,
+  )..repeat();
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  WorldStatesViewModel newsListViewModel = WorldStatesViewModel();
+
+  final colorList = <Color>[
+    Color(0xff4285F4),
+    Color(0xff1aa260),
+    Color(0xffde5246),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return WillPopScope(
+      onWillPop: () async {
+        SystemNavigator.pop();
+        return true;
+      },
+      child: Scaffold(
+        body: SafeArea(
+          child: Padding(
+            padding: const EdgeInsets.all(15.0),
+            child: Column(
+              children: [
+                SizedBox(
+                  height: MediaQuery.of(context).size.height * .01,
+                ),
+                FutureBuilder(
+                    future: newsListViewModel.fetchWorldRecords(),
+                    builder:
+                        (context, AsyncSnapshot<WorldStatesModel> snapshot) {
+                      if (!snapshot.hasData) {
+                        return Expanded(
+                          flex: 1,
+                          child: SpinKitFadingCircle(
+                            color: Colors.white,
+                            size: 50.0,
+                            controller: _controller,
+                          ),
+                        );
+                      } else {
+                        return Column(
+                          children: [
+                            PieChart(
+                              dataMap: {
+                                "Total": double.parse(
+                                    snapshot.data!.cases!.toString()),
+                                "Recovered": double.parse(
+                                    snapshot.data!.recovered.toString()),
+                                "Deaths": double.parse(
+                                    snapshot.data!.deaths.toString()),
+                              },
+                              animationDuration: Duration(milliseconds: 1200),
+                              chartLegendSpacing: 32,
+                              chartRadius:
+                                  MediaQuery.of(context).size.width / 3.2,
+                              colorList: colorList,
+                              initialAngleInDegree: 0,
+                              chartType: ChartType.ring,
+                              ringStrokeWidth: 25,
+                              legendOptions: const LegendOptions(
+                                showLegendsInRow: false,
+                                legendPosition: LegendPosition.left,
+                                showLegends: true,
+                                legendTextStyle: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                              chartValuesOptions: const ChartValuesOptions(
+                                showChartValueBackground: true,
+                                showChartValues: true,
+                                showChartValuesInPercentage: true,
+                                showChartValuesOutside: true,
+                                decimalPlaces: 1,
+                              ),
+                            ),
+                            Padding(
+                              padding: EdgeInsets.symmetric(
+                                  vertical:
+                                      MediaQuery.of(context).size.height * .06),
+                              child: Card(
+                                child: Column(
+                                  children: [
+                                    ReusableRow(
+                                        title: 'Total Cases',
+                                        tColor: Colors.blue,
+                                        value: snapshot.data!.cases.toString()),
+                                    ReusableRow(
+                                        title: 'Deaths',
+                                        tColor: Colors.red,
+                                        value:
+                                            snapshot.data!.deaths.toString()),
+                                    ReusableRow(
+                                        title: 'Recovered',
+                                        tColor: Colors.green,
+                                        value: snapshot.data!.recovered
+                                            .toString()),
+                                    ReusableRow(
+                                        title: 'Active',
+                                        value:
+                                            snapshot.data!.active.toString()),
+                                    ReusableRow(
+                                        title: 'Critical',
+                                        value:
+                                            snapshot.data!.critical.toString()),
+                                    ReusableRow(
+                                        title: 'Today Deaths',
+                                        tColor: Colors.purple,
+                                        value: snapshot.data!.todayDeaths
+                                            .toString()),
+                                    ReusableRow(
+                                        title: 'Today Recovered',
+                                        value: snapshot.data!.todayRecovered
+                                            .toString()),
+                                  ],
+                                ),
+                              ),
+                            ),
+                            GestureDetector(
+                              onTap: () {
+                                Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                        builder: (context) =>
+                                            const CountriesListScreen()));
+                              },
+                              child: Container(
+                                height: 50,
+                                decoration: BoxDecoration(
+                                    color: Colors.pink,
+                                    borderRadius: BorderRadius.circular(10)),
+                                child: const Center(
+                                  child: Text('Track Countries'),
+                                ),
+                              ),
+                            )
+                          ],
+                        );
+                      }
+                    }),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class ReusableRow extends StatelessWidget {
+  String title, value;
+  Color? tColor;
+  ReusableRow({
+    Key? key,
+    required this.title,
+    required this.value,
+    this.tColor,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(left: 10, right: 10, top: 10, bottom: 5),
+      child: Column(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                title,
+                style: TextStyle(color: tColor ?? Colors.black),
+              ),
+              Text(value)
+            ],
+          ),
+          SizedBox(
+            height: 5,
+          ),
+          Divider()
+        ],
+      ),
+    );
+  }
+}
